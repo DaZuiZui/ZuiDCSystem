ceade180997bf95764dec526a708e9871c93b137
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-08-11 13:47:24 +0800

    代码去重测试

---
+++ src/test/java/com/example/duplicatechecksystem/DuplicateCheckSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.duplicatechecksystem;
+
+import com.hankcs.hanlp.HanLP;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicateCheckSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
